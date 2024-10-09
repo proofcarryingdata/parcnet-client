826e34854427b99f31939aa0be64114e050847b0
Rob Knight <rob@0xparc.org>
2024-10-09 15:14:21 -0700

    WIP

---
+++ packages/app-connector/src/connection_state.ts
@@ -0,0 +1,25 @@
+export type ClientConnectionInfo = {
+  url: string;
+  type: "iframe";
+};
+
+export enum ClientConnectionErrorType {
+  PERMISSION_DENIED = "PERMISSION_DENIED"
+}
+
+export type ClientConnectionError = {
+  type: ClientConnectionErrorType.PERMISSION_DENIED;
+  message: string;
+};
+
+export type ClientConnectionState =
+  | {
+      type: "connecting";
+    }
+  | {
+      type: "connected";
+    }
+  | {
+      type: "disconnected";
+      error?: ClientConnectionError;
+    };
