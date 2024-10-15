f7e724184b240f19a9213f0b98762c782609719d
Rob Knight <rob@0xparc.org>
2024-10-15 16:06:19 -0700

    more WIP

--- packages/app-connector-react/src/index.ts
+++ packages/app-connector-react/src/index.ts
@@ -1,10 +1,1 @@
-import { createComponent } from "@lit/react";
-import { ParcnetToolbar } from "@parcnet-js/app-connector/components/parcnet-toolbar";
-import React from "react";
-
-export const ParcnetToolbarComponent = createComponent({
-  tagName: "parcnet-toolbar",
-  elementClass: ParcnetToolbar,
-  react: React,
-  events: {}
-});
+export interface AppConnectorContext {}
