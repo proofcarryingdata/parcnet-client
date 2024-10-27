32b3883bb724e94d6adaaf9d8a34c18f73374710
Rob Knight <rob@0xparc.org>
2024-10-27 16:46:47 +0700

    Fix app-connector test

--- packages/app-connector/test/utils.ts
+++ packages/app-connector/test/utils.ts
@@ -1,5 +1,5 @@
 import { RPCMessageType } from "@parcnet-js/client-rpc";
-import { expect } from "vitest";
+import { assert, expect } from "vitest";
 import type { DialogController } from "../src/adapters/iframe.js";
 import { postRPCMessage } from "../src/adapters/iframe.js";
 import { ParcnetRPCConnector } from "../src/rpc_client.js";
@@ -19,10 +19,16 @@ export async function connectedClient(): Promise<{
 }> {
   const chan = new MessageChannel();
   const client = new ParcnetRPCConnector(chan.port2, mockDialog);
-  client.start(() => {
-    // This is called when the connection is established
-    expect(client.isConnected()).to.be.true;
-  });
+  client.start(
+    () => {
+      // This is called when the connection is established
+      expect(client.isConnected()).to.be.true;
+    },
+    () => {
+      // This is called if the connection fails. We should never get here.
+      assert(false);
+    }
+  );
 
   postRPCMessage(chan.port1, {
     type: RPCMessageType.PARCNET_CLIENT_READY
